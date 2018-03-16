e2ab68e9684a058374fca84c085c3748f7c5d0eb
legitstack <jordan.kay@gmail.com>
2018-03-16 11:24:27 -0600

    pip install issues

--- env.cmd
+++ env.cmd
@@ -2,6 +2,7 @@
 
 doskey a=atom .
 doskey e=explorer .
+doskey x=exit
 doskey s=cd C:\sites\
 doskey r=cd C:\repos\
 doskey w=cd C:\workspaces\
@@ -13,7 +14,7 @@ doskey home=c:
 
 doskey gs=git status
 doskey ga=git add --all
-doskey gc=if $1. equ . (git commit -m "commit") else (git commit -m "$*")
+doskey gc=if $1. equ '' (git commit -m "commit") else (git commit -m "$*")
 doskey gu=git push -u origin master
 doskey gg=if $1. equ '' (git push) else (git push -u origin master)
 doskey gp=git pull
@@ -26,12 +27,9 @@ doskey dos=notepad.exe c:\windows\system32\env.cmd
 doskey commands=notepad.exe c:\windows\system32\env.cmd
 
 REM doskey v=echo 1
-
 REM doskey vu=vagrant up
 REM doskey vs=vagrant ssh
-
 REM doskey dps=mix deps.get
 REM doskey ism=iex -S mix
 REM doskey isms=iex -S mix phx.Server
-
 REM doskey touch=echo. 2>empty_file.txt
